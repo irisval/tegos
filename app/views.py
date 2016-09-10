a14f869e07354bcd79b6398c862124cb3a580c6a
Iris Kim <Iris Kim>
2016-09-10 17:35:26 -0400

    hallelujah this garbage code is displaying the tweets

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request
 from app import app
 from app import t
+from twython import Twython
 from .forms import FilterForm
 
 
@@ -24,13 +25,13 @@ def display():
   else:
     hasht = request.form['hashtag']
   loc = request.form['location']
-  print("hello")
-  search_results = t.search(q="%23"+str(hasht))
-  print(len(search_results))
 
+  search_results = t.search(q="%23" + hasht, result_type='recent')
 
+  str_id_list = []
+  for tweet in search_results["statuses"]:
+    str_id_list.append(tweet["id_str"])
 
+  return render_template('map.html', form=form, hasht = hasht, loc = loc, search_results=str_id_list)
 
-
-  return render_template('map.html', form=form, hasht = hasht, loc = loc)
 
