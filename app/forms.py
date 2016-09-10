482ec0a0809586823d2b3e7545d88bc77a0dd12c
Iris Kim <Iris Kim>
2016-09-10 18:33:17 -0400

    made garbage form changes

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import Form
-from wtforms import TextField, StringField
+from wtforms import TextField, StringField, BooleanField
 from wtforms.validators import DataRequired
 
 class FilterForm(Form):
 	hashtag = StringField('Hashtag', validators=[DataRequired()])
 	location = StringField('Location', validators=[DataRequired()])
+	mapDisplay = BooleanField('Area Choice', description="Show data specific to your current location?", validators=[DataRequired()])
 
 	
--- app/views.py
+++ app/views.py
@@ -19,19 +19,26 @@ def submit():
 
 @app.route('/display', methods=['GET', 'POST'])
 def display():
+
   form = FilterForm()
   if request.form['hashtag'][0] == "#":
     hasht = request.form['hashtag'][1:]
   else:
     hasht = request.form['hashtag']
-  loc = request.form['location']
+  # loc = request.form['location']
+  # display = request.form.map_display.data
 
-  search_results = t.search(q="%23" + hasht, result_type='recent')
+
+  search_results = t.search(q="%23" + hasht, result_type='recent', count=50)
 
   str_id_list = []
   for tweet in search_results["statuses"]:
     str_id_list.append(tweet["id_str"])
 
-  return render_template('map.html', form=form, hasht = hasht, loc = loc, search_results=str_id_list)
+  return render_template('map.html', form=form, hasht = hasht, search_results=str_id_list)
+
+
+
+
 
 
