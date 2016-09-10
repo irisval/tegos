ee836b102114bec5d22f17a971162eedc88b2973
Iris Kim <Iris Kim>
2016-09-10 08:56:15 -0400

    basic routing done

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from .forms import FilterForm
 
@@ -10,9 +10,18 @@ def index():
 
 
 @app.route('/submit', methods=['GET', 'POST'])
-def submit_fields():
-    form = FilterForm()
-    hasht = form.hashtag.data
-    loc = form.location.data
+def submit():
+  form = FilterForm()
+  return render_template('login.html', form=form)
 
-    return render_template('login.html', form=form)
+@app.route('/display', methods=['GET', 'POST'])
+def display():
+  form = FilterForm()
+  if request.form['hashtag'][0] == "#":
+    hasht = request.form['hashtag']
+  else:
+    hasht = "#" + request.form['hashtag']
+  loc = request.form['location']
+
+  return render_template('map.html', form=form, hasht = hasht, loc = loc)
+
